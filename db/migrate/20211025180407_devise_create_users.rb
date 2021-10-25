019d4cfaf222b392faeee309540274ab25033353
takako0420 <marl0420boro@gmail.com>
2021-10-26 03:11:01 +0900

    userテーブルの作成

--- db/migrate/20211025180407_devise_create_users.rb
+++ db/migrate/20211025180407_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name
+      t.text   :profile
+      t.text   :occupation
+      t.text   :position
 
       ## Recoverable
       t.string   :reset_password_token
