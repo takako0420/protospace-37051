c9412d129aa09033dac3795ccb379894a8eb3c6a
takako0420 <marl0420boro@gmail.com>
2021-10-26 05:11:27 +0900

    prototypesのルーティングを設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,6 +3,12 @@ class PrototypesController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+  
   #private
  # def prototype_params
   #  params.require(:Prototype).permit(:content, :image)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'  
-  get 'prototypes/index'
+  #get 'prototypes/index'
+  resources :prototypes, only: [:new, :create]
 end
