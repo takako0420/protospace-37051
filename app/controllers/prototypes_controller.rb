c824498a09e849272d990d395a46d699d4f0eda9
takako0420 <marl0420boro@gmail.com>
2021-10-26 05:00:55 +0900

    Prototypeのバリデーションの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,11 @@
 class PrototypesController < ApplicationController
+  
   def index
   end
+
+  #private
+ # def prototype_params
+  #  params.require(:Prototype).permit(:content, :image)
+  #end
+
 end
